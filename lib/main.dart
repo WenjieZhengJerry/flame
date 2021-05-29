efcd003a9c9dc01e409f52b5f15d463d000761e9
LAPTOP-NPIOT4H9\Jerry <2388623483@qq.com>
2021-05-29 21:21:23 +0800

    feat: 添加动画

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,103 @@
+import 'package:flame/flame.dart';
 import 'package:flame/util.dart';
 import 'package:flame_demo/my-game.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
+  ///这四行是干嘛的不大清楚，锁定屏幕和全屏化
   WidgetsFlutterBinding.ensureInitialized();
-
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+  ///导入图片
+  Flame.images.loadAll(<String>[
+    'man.jpg',
+    'girl/1.png',
+    'girl/2.png',
+    'girl/3.png',
+    'girl/4.png',
+    'girl/5.png',
+    'girl/6.png',
+    'girl/7.png',
+    'girl/8.png',
+    'girl/9.png',
+    'girl/10.png',
+    'girl/11.png',
+    'girl/12.png'
+  ]);
 
   MyGame myGame = MyGame();
+  ///下面三行是监听用户手势并绑定事件
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = myGame.onTapDown;
+  flameUtil.addGestureRecognizer(tapper);
+
   runApp(myGame.widget);
 }
+
+///到时研究一下怎么把动画嵌入到wight中
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   MyGame myGame = MyGame();
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Container(
+//               height: 200,
+//               width: 200,
+//               color: Colors.blue,
+//               child: myGame.widget
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
--- lib/my-game.dart
+++ lib/my-game.dart
@@ -2,19 +2,91 @@ import 'dart:ui';
 
 import 'package:flame/game.dart';
 import 'package:flame/game/base_game.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/cupertino.dart';
 
 class MyGame extends BaseGame {
+  // 屏幕尺寸
   Size screenSize;
+  // 控制举行是否变色
+  bool hasWon = false;
+  // 组图的下标
+  int count = 0;
+  // 组图
+  List<Sprite> girls = [
+    Sprite('girl/1.png'),
+    Sprite('girl/2.png'),
+    Sprite('girl/3.png'),
+    Sprite('girl/4.png'),
+    Sprite('girl/5.png'),
+    Sprite('girl/6.png'),
+    Sprite('girl/7.png'),
+    Sprite('girl/8.png'),
+    Sprite('girl/9.png'),
+    Sprite('girl/10.png'),
+    Sprite('girl/11.png'),
+    Sprite('girl/12.png'),
+  ];
+  // 帧
+  int frame = 0;
 
   void render(Canvas canvas) {
     super.render(canvas);
+    ///下面这段是给整个屏幕铺一层背景色
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xfffbf8e7);
+    canvas.drawRect(bgRect, bgPaint);
+
+    ///下面这段注释得是在屏幕中央画一个白色得正方形
+    // double screenCenterX = screenSize.width / 2;
+    // double screenCenterY = screenSize.height / 2;
+    // Rect boxRect = Rect.fromLTWH(
+    //     screenCenterX - 75,
+    //     screenCenterY - 75,
+    //     150,
+    //     150
+    // );
+    // Paint boxPaint = Paint();
+    // // 通过hasWon得值可以改变正方形得颜色
+    // if (hasWon) {
+    //   boxPaint.color = Color(0xff00ff00);
+    // } else {
+    //   boxPaint.color = Color(0xffffffff);
+    // }
+    // canvas.drawRect(boxRect, boxPaint);
+
+    ///通过canvas在屏幕指定位置渲染3个图
+    canvas.drawImage(girls[count % 12].image, Offset(0, 0), Paint());
+    canvas.drawImage(girls[count % 12].image, Offset((screenSize.width / 2) - 60, (screenSize.height / 2) - 100), Paint());
+    canvas.drawImage(girls[count % 12].image, Offset(screenSize.width - 120, screenSize.height - 200), Paint());
+  }
+
+  ///用于监听用户是否触碰到正方形得区域
+  void onTapDown(TapDownDetails d) {
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    if (d.globalPosition.dx >= screenCenterX - 75
+        && d.globalPosition.dx <= screenCenterX + 75
+        && d.globalPosition.dy >= screenCenterY - 75
+        && d.globalPosition.dy <= screenCenterY + 75
+    ) {
+      hasWon = !hasWon;
+    }
   }
 
   void update(double t) {
     super.update(t);
+
+    // 每5帧，图片下标+1
+    if (frame % 5 == 0) {
+      count++;
+    }
+    frame++;
   }
 
   void resize(Size size) {
     super.resize(size);
+    screenSize = size;
   }
 }
